3ca9e85655ffec3ee10d08ff4c11f0da5143673f
Moein Rasouli <93027482+itsRoutine@users.noreply.github.com>
2023-06-03 03:39:22 +0330

    Fix Contexts constructors

--- src/Persistance/MessageContext.java
+++ src/Persistance/MessageContext.java
@@ -1,9 +1,10 @@
 package Persistance;
 
+import Model.Message;
 import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
 
 public class MessageContext<T> extends DbContext<T> {
-    MessageContext(Class<T> clazz, JdbcPooledConnectionSource connectionSource) {
-        super(clazz, connectionSource);
+    MessageContext(JdbcPooledConnectionSource connectionSource) {
+        super((Class<T>) Message.class, connectionSource);
     }
 }
--- src/Persistance/UserContext.java
+++ src/Persistance/UserContext.java
@@ -1,9 +1,10 @@
 package Persistance;
 
+import Model.User;
 import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
 
 public class UserContext<T> extends DbContext<T>{
-    public UserContext(Class<T> clazz, JdbcPooledConnectionSource connectionSource) {
-        super(clazz, connectionSource);
+    public UserContext(JdbcPooledConnectionSource connectionSource) {
+        super((Class<T>) User.class, connectionSource);
     }
 }
