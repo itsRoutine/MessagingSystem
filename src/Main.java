e53a389ee63ef6601caed3d3aa89cdd3b17ad403
Moein Rasouli <93027482+itsRoutine@users.noreply.github.com>
2023-06-12 16:34:45 +0330

    Add Main function

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
-import Model.User;
 import Persistence.DB;
 import View.Login;
 import com.formdev.flatlaf.FlatLaf;
-import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatArcDarkContrastIJTheme;
-
-import java.util.List;
+import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMaterialDarkerContrastIJTheme;
 
 public class Main {
     public static void main(String[] args) {
         DB db = new DB();
 
-        FlatLaf theme = new FlatArcDarkContrastIJTheme();
+        FlatLaf theme = new FlatMaterialDarkerContrastIJTheme();
         FlatLaf.setup(theme);
 
         new Login(db);
-
     }
 }
